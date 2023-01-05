b373bc9dee27adbb0b9e465e8ad66750843ac320
Joe King <tiptop1112@gmail.com>
2023-01-05 00:33:07 +0000

    Completed feature issue/feature 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // query selecting the input element type text that holds the value of the anime title entered by the user
 var $animeTitle = document.querySelector('#anime');
-console.log($animeTitle);
 // a function to create a new XMLHttpRequest to get anime data based on the string value of the input element above
 var animeDataReq = new XMLHttpRequest();
 function getReq(title) {
@@ -14,9 +13,36 @@ var $animeForm = document.getElementById('searchanime');
 $animeForm.addEventListener('submit', function (event) {
   event.preventDefault();
   getReq($animeTitle.value);
-  // animeDataReq.addEventListener('load', function () {
-  //   console.log('animeDataReq.status is', animeDataReq.status);
-  //   console.log('animeDataReq.response is', animeDataReq.response);
-  // });
-  console.log('Form submitted');
+  $thumbnailContainer.textContent = '';
+});
+
+// query select div element that will be the container of anime thumbnails
+var $thumbnailContainer = document.querySelector('.thumbnail-container');
+
+// create a function that will create new dom elements that will become thumbnails of searched anime title
+function renderThumbnails(anime) {
+  var $colThirdDiv = document.createElement('div');
+  $colThirdDiv.className = 'col-third';
+  var $thumbnail = document.createElement('div');
+  $thumbnail.className = 'thumbnail';
+  $colThirdDiv.appendChild($thumbnail);
+  var $thumbnailTitle = document.createElement('div');
+  $thumbnailTitle.className = 'thumbnail-title';
+  $thumbnail.appendChild($thumbnailTitle);
+  var $h3Title = document.createElement('h3');
+  $h3Title.className = 'h3-title';
+  $h3Title.textContent = anime.title;
+  $thumbnailTitle.appendChild($h3Title);
+  var $thumbnailImg = document.createElement('img');
+  $thumbnailImg.setAttribute('src', anime.images.jpg.large_image_url);
+  $thumbnailImg.className = 'thumbnail-img';
+  $thumbnail.appendChild($thumbnailImg);
+  return $colThirdDiv;
+}
+// add an addEventlistener for the load event to update the dom to create thumbnails of the anime the user searched for
+animeDataReq.addEventListener('load', function () {
+  for (var i = 0; i < animeDataReq.response.data.length; i++) {
+    var tbRender = renderThumbnails(animeDataReq.response.data[i]);
+    $thumbnailContainer.appendChild(tbRender);
+  }
 });
